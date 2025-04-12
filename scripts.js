81b0bfe32ac21f461fa9a4d7b6bd884440e67021
JeanSousa <jeanjr.silvasousa@gmail.com>
2025-04-12 17:56:56 -0300

    add create product method

--- scripts.js
+++ scripts.js
@@ -2,7 +2,52 @@ function toggleSidebar() {
     document.getElementById('sidebar').classList.toggle('collapsed');
 }
 
+
+
+const home = () => {
+    document.getElementById('home-container').classList.remove('d-none');
+    document.getElementById('product-container').classList.add('d-none')
+}
+
+/*
+    --------------------------------------------------------------------------------------
+    Prefix url to send request to backend
+    --------------------------------------------------------------------------------------
+*/
 const prefixUrl = "http://127.0.0.1:5000/api";
+
+
+/*
+    --------------------------------------------------------------------------------------
+    Function to get the list of products from the server via GET request
+    --------------------------------------------------------------------------------------
+*/
+const createProduct = async () => {
+    let url = `${prefixUrl}/products`;
+    let name = document.getElementById('product-name').value
+    let value = document.getElementById('product-value').value
+
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('value', value);
+
+    try {
+        let response = await fetch(
+            url, 
+            {
+                method: 'post',
+                body: formData,
+            }
+        )
+
+        console.log(response)
+
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
 
 /*
     --------------------------------------------------------------------------------------
@@ -12,26 +57,53 @@ const prefixUrl = "http://127.0.0.1:5000/api";
 const listProducts = async () => {
     let url = `${prefixUrl}/products`;
 
-    document.getElementById('home-container').remove();
+    document.getElementById('home-container').classList.add('d-none');
     document.getElementById('product-container').classList.remove('d-none')
 
     try {
         let response = await fetch(url, {method: 'get'})
-
         let { products } = await response.json()
 
+        const tableBody = document.getElementById('my-table')
+            .getElementsByTagName('tbody')[0];
+
+        tableBody.innerHTML = "";
+
+        if(!products) {
+            alert('Não existem produtos na base de dados');
+        }
+
         products.forEach(item => {insrtProductItem(item.id, item.name, item.value)})
-    
+
     } catch (error) {
         console.log(error)
     }
 }
 
-function deleteProduct(button) {
-    console.log(button);
-    const row = button.closest("tr");
-    row.remove();
+/*
+    --------------------------------------------------------------------------------------
+    Function to delete product from the server via DELETE request
+    --------------------------------------------------------------------------------------
+*/
+const deleteProduct = async (id) => {
+    let url = `${prefixUrl}/products/${id}`;
+
+    try {
+        let response = await fetch(url, {method: 'delete'})
+
+        let data = await response.json()
+
+        if (data.message) {
+            alert(`Produto ${data.name} deletado com sucesso!`);
+        }
+
+        document.getElementById(`product-${id}`).remove()    
+    } catch (error) {
+        console.log(error)
+    }
 }
+
+
 
 
 
@@ -43,6 +115,7 @@ const insrtProductItem = (id, name, value) => {
         .getElementsByTagName('tbody')[0];
 
     const row = table.insertRow();
+    row.setAttribute("id", `product-${id}`); // ID único para a linha
 
     // Insert name and value in table
     [name, value].forEach(content => {
@@ -50,7 +123,7 @@ const insrtProductItem = (id, name, value) => {
         cell.textContent = content;
     });
 
-    // Insert cell and button
+    // Insert action buttons
     const actionCell = row.insertCell();
     actionCell.innerHTML = `
         <button class="btn btn-sm btn-primary me-2" onclick="editProduct(${id})">
